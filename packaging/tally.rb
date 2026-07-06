373422927d25fc78fa7f3e990a19ba40e6c3e65f
mfwolffe <mfwolffe@outlook.com>
2026-07-05 20:39:00 -0400

    packaging: pin v0.1.1 sha256

--- packaging/tally.rb
+++ packaging/tally.rb
@@ -2,7 +2,7 @@ class Tally < Formula
   desc "GNU wc reimplementation: byte-identical output, SIMD word counting"
   homepage "https://github.com/tenseleyFlow/tally"
   url "https://github.com/tenseleyFlow/tally/releases/download/v0.1.1/tally-0.1.1.tar.gz"
-  sha256 "PINNED_AT_RELEASE"
+  sha256 "65eb5860c9e07b217f212fcec2301ef9d30e3b649b7315632b075ebadb051ddd"
   license "GPL-3.0-or-later"
   head "https://github.com/tenseleyFlow/tally.git", branch: "trunk"
 
